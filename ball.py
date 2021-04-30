3c4c60d918e749485350339b4e7cdbf4189cf253
Milthon-cyber <me24lt@gmail.com>
2021-04-30 10:49:22 -0600

    tres vidas

--- ball.py
+++ ball.py
@@ -1,18 +1,9 @@
 import pygame as pg
 import sys
-from random import randint
+from random import randint, choice
 
-def rebotaX(x):
-    if x <=0 or x >=ANCHO:
-        return -1
-    
-    return 1
 
-def rebotaY(y):
-    if y <=0 or y >=ALTO:
-        return -1
-    
-    return 1
+
 
 
 ROJO = (255, 0, 0)
@@ -23,31 +14,92 @@ ANCHO = 800
 ALTO = 600
 
 pg.init()
-pantalla = pg.display.set_mode((ANCHO,ALTO))
+pantalla = pg.display.set_mode((ANCHO, ALTO))
 reloj = pg.time.Clock()
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color):
+    def __init__(self, x, y, vx=5, vy=5, color= (255, 255, 255), radio=10):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.color = color
+        self.anchura = radio*2
+        self.altura = radio*2
+
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.y <=0:
+            self.vy = -self.vy
+        
+        if self.x <=0 or self.x >=ANCHO:
+            self.vx = -self.vx
+
+        if self.y >=ALTO:
+            self.x = ANCHO // 2
+            self.y = ALTO // 2
+            self.vx = randint(5,10)*choice([-1, 1])
+            self.vy = randint(5,10)*choice([-1, 1])
+            return True
+        return False
+
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.anchura//2)
+
+    def comprueba_colision(self, objeto):
+        '''
+        if self.x >= objeto.x and self.x <= objeto.x+objeto.anchura or \
+           self.x+self.anchura >= objeto.x and self.x+self.anchura <= objeto.x + objeto.anchura:
+            choqueX = True
+        else:
+            choqueX = False
+        '''
+
+        choqueX = self.x >= objeto.x and self.x <= objeto.x+objeto.anchura or \
+           self.x+self.anchura >= objeto.x and self.x+self.anchura <= objeto.x + objeto.anchura
+        choqueY = self.y >= objeto.y and self.y <= objeto.y+objeto.altura or \
+           self.y+self.altura >= objeto.y and self.y+self.altura <= objeto.y + objeto.altura
+
+        if choqueX and choqueY:
+            self.vy *= -1
 
 
+class Raqueta():
+    def __init__(self, x=0, y=0):
+        self.altura = 10
+        self.anchura = 100
+        self.color = (255, 255, 255)
+        self.x = (ANCHO - self.anchura) // 2
+        self.y = ALTO - self.altura - 15
+        self.vy = 0
+        self.vx = 13
 
-bolas = []
-for _ in range(10):
-    bola = Bola(randint(0, ANCHO),
-                randint(0, ALTO),
-                randint(5, 10),
-                randint(5, 10),
-                (randint(0, 255), randint(0, 255), randint(0,255)))
+    def dibujar(self, lienzo):
+        rect = pg.Rect(self.x, self.y, self.anchura, self.altura)
+        pg.draw.rect(lienzo, self.color, rect)
 
-    bolas.append(bola)
+    def actualizar(self):
+        teclas_pulsadas = pg.key.get_pressed() 
+
+        if teclas_pulsadas[pg.K_LEFT] and self.x > 0:
+            self.x -= self.vx
+        if teclas_pulsadas[pg.K_RIGHT] and self.x < ANCHO - self.anchura:
+            self.x += self.vx
+
+
+vidas = 3
+bola = Bola(randint(0, ANCHO),
+            randint(0, ALTO),
+            randint(5, 10)*choice([-1, 1]),
+            randint(5, 10)*choice([-1, 1]),
+            AZUL)
+
+raqueta = Raqueta()
 
 game_over = False
-while not game_over:
+while not game_over and vidas > 0:
     v = reloj.tick(60)
     #Gestion de eventos
     for evento in pg.event.get():
@@ -55,21 +107,21 @@ while not game_over:
             game_over = True
 
     # Modificación de estado
-    for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
-
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x) 
-    
+    raqueta.actualizar()
+    pierdebola = bola.actualizar()
+    if pierdebola:
+        vidas -= 1
+    bola.comprueba_colision(raqueta)
 
     # Gestión de la pantalla
     pantalla.fill(NEGRO)
-    for bola in bolas:
-        pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
+    bola.dibujar(pantalla)
+    raqueta.dibujar(pantalla)
 
-    
-    pg.display.flip()    
+
+    pg.display.flip()
+    if pierdebola:
+        pg.time.delay(500)
 
 pg.quit()
 sys.exit()
